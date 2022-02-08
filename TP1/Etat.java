06f3c59971d76bd417e2b0a16e6334fd473e0d09
unknown <65182431+RaphaelJ-S@users.noreply.github.com>
2022-02-08 13:10:44 -0500

    petit changements de lint et corrections de warning

--- TP1/Etat.java
+++ TP1/Etat.java
@@ -9,13 +9,13 @@
 import java.util.*;
 import java.awt.geom.Point2D;
 
-
 public class Etat implements Comparable<Etat> {
 
     // Référence sur la situation du Ramassage
     protected Ramassage ramassage;
 
-    // Noyau de la représentation d'un état. Ici, on met tout ce qui rend l'état unique.
+    // Noyau de la représentation d'un état. Ici, on met tout ce qui rend l'état
+    // unique.
     /* Emplacement du van. */
     protected Emplacement emplacementVan;
     /* Array indicant l'emplacement de chaque colis. */
@@ -27,87 +27,99 @@ public class Etat implements Comparable<Etat> {
 
     // Variables pour l'algorithme A*.
     /* État précédent permettant d'atteindre cet état. */
-    protected Etat            parent;
+    protected Etat parent;
     /* Action à partir de parent permettant d'atteindre cet état. */
-    protected String          actionFromParent;
+    protected String actionFromParent;
     /* f=g+h. */
-    protected double          f;
-    /* Meilleur coût trouvé pour atteindre cet état à partir de l'état initial. */    
-    protected double          g;
+    protected double f;
+    /* Meilleur coût trouvé pour atteindre cet état à partir de l'état initial. */
+    protected double g;
     /* Estimation du coût restant pour atteindre le but. */
-    protected double          h;
+    protected double h;
 
-    
-    public Etat(final Ramassage ramassage){
+    public Etat(final Ramassage ramassage) {
         this.ramassage = ramassage;
     }
 
-    /** Fonction retournant les états successeurs à partir de cet état.
-     *  Aussi appelé fonction de transition.
-     *  Cela permet d'explorer l'espace d'état (le graphe de recherche).
+    /**
+     * Fonction retournant les états successeurs à partir de cet état.
+     * Aussi appelé fonction de transition.
+     * Cela permet d'explorer l'espace d'état (le graphe de recherche).
      */
     // À compléter.
     //
-    //   - Pour chaque action possible :
-    //   - Instanciez un objet Successeur s;
-    //   - Copiez l'état courant (s.etat = clone()).
-    //   - Créez une chaîne de caractère représentant l'action (s.action).
-    //   - Affectez le coût de cette action (s.cout). 
-    //   - Changez la valeur des variables appropriées dans s.etat.
-    //   - Ajoutez s dans la liste successeurs.
+    // - Pour chaque action possible :
+    // - Instanciez un objet Successeur s;
+    // - Copiez l'état courant (s.etat = clone()).
+    // - Créez une chaîne de caractère représentant l'action (s.action).
+    // - Affectez le coût de cette action (s.cout).
+    // - Changez la valeur des variables appropriées dans s.etat.
+    // - Ajoutez s dans la liste successeurs.
 
-    public Collection<Successeur> enumererEtatsSuccesseurs()
-    {
+    public Collection<Successeur> enumererEtatsSuccesseurs() {
         final LinkedList<Successeur> successeurs = new LinkedList<Successeur>();
         final ArrayList<Route> deplacementsPossibles = this.emplacementVan.routes;
         final String type = this.emplacementVan.type;
-        if(type.equals("C") && !colisEstCharge()) {
-            successeurs.add(new Successeur(chargerColis(), "C", this.ramassage.dureeChargement));
-        }else if(type.equals("A")){
+        if (type.equals("C") && !colisEstCharge()) {
+            successeurs.add(new Successeur(
+                    chargerColis(),
+                    "C",
+                    this.ramassage.dureeChargement));
+        } else if (type.equals("A")) {
             int indexColis = peutDecharger();
-            if(indexColis != -1)
-                successeurs.add(new Successeur(dechargerColis(indexColis), "A", this.ramassage.dureeDechargement));
+            if (indexColis != -1)
+                successeurs.add(new Successeur(
+                        dechargerColis(indexColis),
+                        "A",
+                        this.ramassage.dureeDechargement));
         }
-        for(Route aEffectuer : deplacementsPossibles) {
-            successeurs.add(new Successeur(effectuerDeplacement(aEffectuer), aEffectuer.destination.type, aEffectuer.destination.type.equals("-") ? 2 : 1));
+        for (Route aEffectuer : deplacementsPossibles) {
+            successeurs.add(new Successeur(
+                    effectuerDeplacement(aEffectuer),
+                    aEffectuer.destination.type,
+                    aEffectuer.destination.type.equals("-") ? 2 : 1));
         }
 
-		
-		// À compléter.
-        
-		//
-        //   - Les actions possibles sont : 
-        //   ----> emprunter une route de l'emplacement courant pour aller sur un emplacement voisin,
-        //   ----> Ramasser un Colis lorsque le Van se trouve sur un emplacement de Colis pas encore transporté
-        //   ----> Decharger un Colis lorsque le Van se trouve une un emplacement d'arrivee  
-        
-        //   - Pour toute action possible 
-        //   --- Instancier un objet Successeur s;
-        //   --- Cloner l'état courant dans la variable état du successeur (s.etat = clone()).
-        //   --- Créer la chaîne de caractère représentant l'action dans s.action (voir plans fournis).
-        //   -----> ex. d'un déplacement à l'ouest  "Ouest = Lieu " +  route.origine.nom + " -> Lieu " + route.destination.nom + ")"       
-        //   --- Calculer le coût de cette action dans s.cout.
-        //   -----> ex. pour un déplacement, le cout est 1 + le cout de l'emplacement
-        //   --- Modifier la valeur des variables appropriées dans s.etat pour refléter l'effet de l'action (qu'est-ce qui change?) 
-        //   --- Ajouter s dans la liste successeurs.
-		
+        // À compléter.
+
+        //
+        // - Les actions possibles sont :
+        // ----> emprunter une route de l'emplacement courant pour aller sur un
+        // emplacement voisin,
+        // ----> Ramasser un Colis lorsque le Van se trouve sur un emplacement de Colis
+        // pas encore transporté
+        // ----> Decharger un Colis lorsque le Van se trouve une un emplacement
+        // d'arrivee
+
+        // - Pour toute action possible
+        // --- Instancier un objet Successeur s;
+        // --- Cloner l'état courant dans la variable état du successeur (s.etat =
+        // clone()).
+        // --- Créer la chaîne de caractère représentant l'action dans s.action (voir
+        // plans fournis).
+        // -----> ex. d'un déplacement à l'ouest "Ouest = Lieu " + route.origine.nom + "
+        // -> Lieu " + route.destination.nom + ")"
+        // --- Calculer le coût de cette action dans s.cout.
+        // -----> ex. pour un déplacement, le cout est 1 + le cout de l'emplacement
+        // --- Modifier la valeur des variables appropriées dans s.etat pour refléter
+        // l'effet de l'action (qu'est-ce qui change?)
+        // --- Ajouter s dans la liste successeurs.
+
         return successeurs;
     }
 
-
     /* Crée un nouvel État en copiant le contenu pertinent de l'état actuel */
     @Override
-    public Etat  clone()
-    {
+    public Etat clone() {
         final Etat etat2 = new Etat(ramassage);
         etat2.colisCharge = colisCharge;
         etat2.emplacementVan = emplacementVan;
         etat2.emplacementsColis = new Emplacement[emplacementsColis.length];
-        for(int i = 0; i< emplacementsColis.length; i++)
+        for (int i = 0; i < emplacementsColis.length; i++)
             etat2.emplacementsColis[i] = emplacementsColis[i];
 
         etat2.colisRecuperes = new boolean[colisRecuperes.length];
-        for(int i = 0; i< colisRecuperes.length; i++)
+        for (int i = 0; i < colisRecuperes.length; i++)
             etat2.colisRecuperes[i] = colisRecuperes[i];
         return etat2;
     }
@@ -117,127 +129,134 @@ public class Etat implements Comparable<Etat> {
     public int compareTo(final Etat o) {
         int c;
         c = this.emplacementVan.compareTo(o.emplacementVan);
-        if(c!=0) return c;
+        if (c != 0)
+            return c;
 
-        if(colisCharge == o.colisCharge)
-            c=0;
-        else if(colisCharge == true)
+        if (colisCharge == o.colisCharge)
+            c = 0;
+        else if (colisCharge == true)
             return 1;
         else
             return -1;
-       
-        for(int i = 0; i< emplacementsColis.length; i++){
-            c = (colisRecuperes[i]?1:0) - (o.colisRecuperes[i]?1:0);
-            if(c!=0) return c;
-            if(!colisRecuperes[i]){
+
+        for (int i = 0; i < emplacementsColis.length; i++) {
+            c = (colisRecuperes[i] ? 1 : 0) - (o.colisRecuperes[i] ? 1 : 0);
+            if (c != 0)
+                return c;
+            if (!colisRecuperes[i]) {
                 c = emplacementsColis[i].compareTo(o.emplacementsColis[i]);
-                if(c!=0) return c;
+                if (c != 0)
+                    return c;
             }
         }
         return 0;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
         String s = "ETAT: f=" + f + "  g=" + g + "\n";
         s += "  Pos=" + emplacementVan.nom + "";
-        for(int i = 0; i< emplacementsColis.length; i++){
+        for (int i = 0; i < emplacementsColis.length; i++) {
             s += "\n  PosColis[i]=";
-            s += emplacementsColis[i]==null ? "--" : emplacementsColis[i].nom;
+            s += emplacementsColis[i] == null ? "--" : emplacementsColis[i].nom;
         }
         s += "\n";
         return s;
     }
 
-    //Détermine si le colis qui se trouve sur la case présente est déjà chargé ou non.
+    // Détermine si le colis qui se trouve sur la case présente est déjà chargé ou
+    // non.
     private boolean colisEstCharge() {
         boolean colisCharge = true;
         int index = trouverIndexColis();
-        if(index != -1) {
+        if (index != -1) {
             colisCharge = this.colisRecuperes[index];
         }
 
         return colisCharge;
     }
-    //Retourne l'index du premier colis à décharger s'il existe.
+
+    // Retourne l'index du premier colis à décharger s'il existe.
     private int peutDecharger() {
         int i = 0;
         int indexColis = -1;
-        while(i < this.colisRecuperes.length && indexColis == -1){
-            if(this.colisRecuperes[i] && this.emplacementsColis[i] != null) {
+        while (i < this.colisRecuperes.length && indexColis == -1) {
+            if (this.colisRecuperes[i] && this.emplacementsColis[i] != null) {
                 indexColis = i;
             }
             i++;
         }
         return indexColis;
     }
-    //Retourne l'index du colis si on se trouve sur une case qui en contient un.
+
+    // Retourne l'index du colis si on se trouve sur une case qui en contient un.
     private int trouverIndexColis() {
-        for(int i = 0; i < this.emplacementsColis.length; i++) {
-            if(this.emplacementsColis[i].id == this.emplacementVan.id) {
+        for (int i = 0; i < this.emplacementsColis.length; i++) {
+            if (this.emplacementsColis[i].id == this.emplacementVan.id) {
                 return i;
             }
         }
         return -1;
     }
-    //Retourne la liste des index des colis chargés ou qui restent à charger selon le paramètre chercheCharger
+
+    // Retourne la liste des index des colis chargés ou qui restent à charger selon
+    // le paramètre chercheCharger
     private List<Integer> trouverTousIndexColis(boolean chercheCharge) {
         ArrayList<Integer> indexColis = new ArrayList<>(this.colisRecuperes.length);
-        for(int i = 0; i< colisRecuperes.length; i++) {
-            if(colisRecuperes[i] == chercheCharge){
+        for (int i = 0; i < colisRecuperes.length; i++) {
+            if (colisRecuperes[i] == chercheCharge) {
                 indexColis.add(i);
             }
         }
         return indexColis;
     }
 
-    //Retourne un Etat ayant effectué un déplacement.
+    // Retourne un Etat ayant effectué un déplacement.
     private Etat effectuerDeplacement(Route deplacement) {
         Etat copie = this.clone();
         copie.emplacementVan = deplacement.destination;
         copie.parent = this;
-        for(Integer index : trouverTousIndexColis(true)) {
+        for (Integer index : trouverTousIndexColis(true)) {
             copie.emplacementsColis[index] = deplacement.destination;
         }
         copie.actionFromParent = deplacement.destination.type;
 
-
         return copie;
     }
 
-    //Retourne un État ayant effectué un chargement de colis.
+    // Retourne un État ayant effectué un chargement de colis.
     private Etat chargerColis() {
         Etat copie = this.clone();
         int index = trouverIndexColis();
-        if(index == -1) return null;
-        //Le colis ne se trouve plus à cet emplacement alors on le remplace par l'emplacement présent.
+        if (index == -1)
+            return null;
+        // Le colis ne se trouve plus à cet emplacement alors on le remplace par
+        // l'emplacement présent.
         copie.emplacementsColis[index] = copie.emplacementVan;
-        //Je ne sais pas si cet attribut a de l'important vu qu'on peut avoir un nombre infini de colis.
+        // Je ne sais pas si cet attribut a de l'important vu qu'on peut avoir un nombre
+        // infini de colis.
         copie.colisRecuperes[index] = true;
         copie.parent = this;
         copie.actionFromParent = "C";
-        //manque le calcul des fonctions f(x) = g(x) + h(x)
+        // manque le calcul des fonctions f(x) = g(x) + h(x)
 
         return copie;
     }
 
-    //Retourne un État ayant effectué un déchargement de colis.
+    // Retourne un État ayant effectué un déchargement de colis.
     private Etat dechargerColis(int indexADecharger) {
         Etat copie = this.clone();
-        //On décharge le premier colis dans la van.
+        // On décharge le premier colis dans la van.
 
         copie.emplacementsColis[indexADecharger] = null;
         copie.parent = this;
         copie.actionFromParent = "A";
-        //manque le calcul des fonctions f(x) = g(x) + h(x)
-
+        // manque le calcul des fonctions f(x) = g(x) + h(x)
 
         return copie;
     }
 
-    
-    public int calculCout(Emplacement e)
-    {
+    public int calculCout(Emplacement e) {
         // int cout = 1;
         // if("-".equals(e.type)) cout = 2;
         return "-".equals(e.type) ? 2 : 1;
--- TP1/TP1.java
+++ TP1/TP1.java
@@ -6,7 +6,6 @@
  * 
  */
 
-
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileWriter;
@@ -25,11 +24,11 @@ public class TP1 {
 	public static void main(String args[]) throws IOException {
 		final ParseurH parseur = new ParseurH();
 		BufferedReader keyboard = new BufferedReader(new InputStreamReader(System.in));
-		String line = ""; 
+		String line = "";
 		System.out.println("Donne le nom du fichier contenant ton mondeH.");
 		System.out.println("Remarque: le fichier doit être à la racine du projet et dois se terminer par .txt");
 		System.out.println("Exemple: ramassageH01.txt");
-		
+
 		// Recuperation du fichier map et parseur du fichier
 		try {
 			line = keyboard.readLine();
@@ -40,37 +39,39 @@ public class TP1 {
 
 		// Création de l'objet évaluateur d'heuristique
 		Heuristique h = new HeuristiqueV1(parseur.ramassage);
-		System.out.println("distance_heuristique(depart,arrivee) = " + h.estimerCoutRestant(parseur.etatInitial, parseur.but));
+		System.out.println(
+				"distance_heuristique(depart,arrivee) = " + h.estimerCoutRestant(parseur.etatInitial, parseur.but));
 
+		// for(Map.Entry<String, Emplacement> elem :
+		// parseur.ramassage.emplacements.entrySet()) {
+		// System.out.println("Element : " + elem.getValue().toString());
+		// for(var entry: elem.getValue().routes) {
+		// System.out.println("de : " + entry.origine.toString() + " -> vers :" +
+		// entry.destination.toString());
+		// }
 
-		
-		// for(Map.Entry<String, Emplacement> elem : parseur.ramassage.emplacements.entrySet()) {
-		// 	System.out.println("Element : " + elem.getValue().toString());
-		// 	for(var entry: elem.getValue().routes) {
-		// 		System.out.println("de : " + entry.origine.toString() + " -> vers :" + entry.destination.toString());
-		// 	}
-			
 		// }
 		parseur.etatInitial.emplacementVan = parseur.etatInitial.ramassage.emplacements.get("1-10");
 
-		List<Successeur> successeurs = (List)parseur.etatInitial.enumererEtatsSuccesseurs();
+		List<Successeur> successeurs = (List<Successeur>) parseur.etatInitial.enumererEtatsSuccesseurs();
 
 		System.out.println(successeurs);
 
-		successeurs = (List)successeurs.get(0).etat.enumererEtatsSuccesseurs();
+		successeurs = (List<Successeur>) successeurs.get(0).etat.enumererEtatsSuccesseurs();
 
 		System.out.println("----");
 		System.out.println(successeurs);
 
-
-		/* Appel à l'algorithme A* : enregistrement du plan dans la List
+		/*
+		 * Appel à l'algorithme A* : enregistrement du plan dans la List
 		 * 
 		 * À la fin de l'appel plan doit intégrer une liste de String donnant
 		 * la suite d'instructions pour effectuer tous les deplacements selon
 		 * le format donné par la chaine de caratères (cf. classe Route)
-		 * "Ouest = Lieu " +  route.origine.nom + " -> Lieu " + route.destination.nom + ")"
+		 * "Ouest = Lieu " + route.origine.nom + " -> Lieu " + route.destination.nom +
+		 * ")"
 		 * 
-		 * la strategie à effectuer est : 
+		 * la strategie à effectuer est :
 		 * Partir de l'entrée (emplacement actuel du Van)
 		 * aller au premier Colis
 		 * ramener le premier Colis au point de Ramassage
@@ -80,8 +81,8 @@ public class TP1 {
 		 */
 		final List<String> plan = AEtoile.composerPlan(parseur.etatInitial, parseur.but, h);
 
-		//ramassageH01.txt
-		//urgenceH01.txt
+		// ramassageH01.txt
+		// urgenceH01.txt
 		// Écriture du plan dans le fichier de sortie + affichage console
 		final File f = new File("planH" + line.substring(10, 12) + ".txt");
 		try {
