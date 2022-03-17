93c6c2c29e019112ebc2cc9cb05f6e80a480ab37
Raphael Jacob-Simard <RaphaelJ-S@hotmail.com>
2022-03-17 17:25:58 -0400

    ajout de contrôle de profondeur pour les conditions

--- Connect5-Aleatoire/src/Connect5Game/ConditionArretToutesCases.java
+++ Connect5-Aleatoire/src/Connect5Game/ConditionArretToutesCases.java
@@ -1,10 +1,17 @@
 package Connect5Game;
 
 public class ConditionArretToutesCases implements ConditionArret {
+    private int profondeurMax = Integer.MAX_VALUE;
 
     @Override
     public boolean conditionArret(Grille grille) {
         return grille.getPositionLibres().size() == 0;
+    }
+
+    @Override
+    public void setProfondeurMax(int profondeurMax) {
+        this.profondeurMax = profondeurMax;
+
     }
 
 }
