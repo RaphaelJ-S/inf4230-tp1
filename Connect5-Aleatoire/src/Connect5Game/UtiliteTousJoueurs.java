3a6227c52d1873e1d03839a0962d51db9a51867e
Francis <FrancisStromei>
2022-03-19 23:18:15 -0400

    Update fonction d'évaluation pour tenir en compte toute les combinaisons possiblement gagnantes

--- Connect5-Aleatoire/src/Connect5Game/UtiliteTousJoueurs.java
+++ Connect5-Aleatoire/src/Connect5Game/UtiliteTousJoueurs.java
@@ -1,6 +1,8 @@
 package Connect5Game;
 
 import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.LinkedList;
 import java.util.HashMap;
 
 public class UtiliteTousJoueurs implements Utilite {
@@ -9,11 +11,11 @@ public class UtiliteTousJoueurs implements Utilite {
         gauche,
         hautGauche,
         haut,
-        hautDroite,
-        droite,
         basGauche,
+        droite,
+        basDroite,
         bas,
-        basDroite
+        hautDroite
     }
 
     /**
@@ -77,65 +79,98 @@ public class UtiliteTousJoueurs implements Utilite {
     // Si une direction n'a pas de possibilité de victoire, à cause d'un jeton de
     // l'adversaire ou une case non-accessible, cette direction vaut 0.
     private int evaluerPosition(Grille grille, Position aEvaluer, int joueurCourant) {
+        final int distanceMaximale = 4; // Distance maximale par rapport à la position à evaluer dans le but d'atteindre une combinaison de 5 jetons et gagner la partie
+        LinkedList<Directions> directionsOpposees = new LinkedList<Directions>(EnumSet.range(Directions.droite, Directions.hautDroite));
         int utilite = 0;
-
-        for (Directions direction : Directions.values()) {
-            int x = aEvaluer.colonne;
-            int y = aEvaluer.ligne;
+    
+        for (Directions direction : EnumSet.range(Directions.gauche, Directions.basGauche)) {
+            Position positionDirection = aEvaluer;
+            Position positionDirectionOpposee = aEvaluer;
+            LinkedList<Position> positionsOpposees = new LinkedList<Position>();
             int distance = 0;
-            int nbJetonsDirection = 0;
+            int distanceOpposee = 0;
+            int nbJetons = 0;
             boolean caseValide = false;
 
+            // On commence par valider les 4 positions dans la direction opposée ainsi que le nombre de jetons alliés s'y trouvant
+            Directions directionOpposee = directionsOpposees.poll(); // On synchronise la nouvelle direction avec sa direction opposée
             do {
-                switch (direction) {
-                    // On commence par ajuster notre curseur sur la prochaine case à valider
-                    case gauche:
-                        --x;
-                        break;
-                    case hautGauche:
-                        --x;
-                        --y;
-                        break;
-                    case haut:
-                        --y;
-                        break;
-                    case hautDroite:
-                        ++x;
-                        --y;
-                        break;
-                    case droite:
-                        ++x;
-                        break;
-                    case basGauche:
-                        --x;
-                        ++y;
-                        break;
-                    case bas:
-                        ++y;
-                        break;
-                    case basDroite:
-                        ++x;
-                        ++y;
-                        break;
-                    default:
-                        break;
+                positionDirectionOpposee = trouverProchainePositionAValider(directionOpposee, positionDirectionOpposee);
+                caseValide = validerCase(grille, positionDirectionOpposee, joueurCourant);
+                if (caseValide) {
+                    positionsOpposees.addFirst(positionDirectionOpposee);
+                    if (grille.get(positionDirectionOpposee) == joueurCourant) {
+                        ++nbJetons;
+                    }
+                    ++distanceOpposee;
                 }
-                ++distance;
-                Position positionAValider = new Position(y, x);
-                caseValide = validerCase(grille, positionAValider, joueurCourant);
+            } while (caseValide && distanceOpposee < distanceMaximale);
+            // On vérifie ensuite toutes les combinaisons gagnante entre une direction et sa direction opposée
+            do {
+                if (distance + distanceOpposee == distanceMaximale)
+                {
+                    // On ajoute une valeur d'utilité uniquement quand il est possible de gagner pour une combinaison (pas de case inaccessible ou utilisée par l'adversaire)
+                    utilite += 1 * Math.pow(10, nbJetons);
+                    if (grille.get(positionsOpposees.poll()) == joueurCourant) {
+                        --nbJetons;
+                    }
+                    --distanceOpposee;
+                }
+
+                positionDirection = trouverProchainePositionAValider(direction, positionDirection);
+                caseValide = validerCase(grille, positionDirection, joueurCourant);
 
                 if (caseValide) {
-                    if (grille.get(positionAValider) == joueurCourant) {
-                        ++nbJetonsDirection;
+                    if (grille.get(positionDirection) == joueurCourant) {
+                        ++nbJetons;
                     }
-                    if (distance == 4) // Direction où il est possible de gagner
-                    {
-                        utilite += 1 * Math.pow(10, nbJetonsDirection);
-                    }
+                    ++distance;
                 }
-            } while (caseValide);
+            } while (caseValide && distance < distanceMaximale);
         }
         return utilite;
+    }
+
+    private Position trouverProchainePositionAValider(Directions direction, Position positionPrecedente)
+    {
+        int x = positionPrecedente.colonne;
+        int y = positionPrecedente.ligne;
+
+        switch (direction) {
+            // On ajuste notre curseur sur la prochaine case à valider
+            case gauche:
+                --x;
+                break;
+            case hautGauche:
+                --x;
+                --y;
+                break;
+            case haut:
+                --y;
+                break;
+            case hautDroite:
+                ++x;
+                --y;
+                break;
+            case droite:
+                ++x;
+                break;
+            case basGauche:
+                --x;
+                ++y;
+                break;
+            case bas:
+                ++y;
+                break;
+            case basDroite:
+                ++x;
+                ++y;
+                break;
+            default:
+                break;
+        }
+
+        return new Position(y, x);
     }
 
     private boolean validerCase(Grille grille, Position aEvaluer, int joueurCourant) {
