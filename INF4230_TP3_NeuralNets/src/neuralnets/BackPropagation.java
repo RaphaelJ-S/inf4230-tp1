1fad129c145961f2cf0a448824a21e7a02aec017
Francis <FrancisStromei>
2022-05-01 16:57:57 -0400

    cleanup ++i

--- INF4230_TP3_NeuralNets/src/neuralnets/BackPropagation.java
+++ INF4230_TP3_NeuralNets/src/neuralnets/BackPropagation.java
@@ -2,7 +2,6 @@ package neuralnets;
 
 
 import java.util.*;
-import static neuralnets.Neuron.derivative;
 
 public class BackPropagation {
 
@@ -20,16 +19,16 @@ public class BackPropagation {
         // handle the output layer first
         double [] outputLayerDeltas = new double[_network.nbrOutputNeurons];
         int i = 0; // pour l'index dans le tableau de deltas puisque neuron_deltas est une liste d'array
-        for(Neuron output : _network.layers[_network.layers.length - 1]) {
+        for(Neuron outputLayer : _network.layers[_network.layers.length - 1]) {
 
-            outputLayerDeltas[i++] = (_expected - output.output) * output.output * (1 - output.output);
+            outputLayerDeltas[i++] = (_expected - outputLayer.output) * outputLayer.output * (1 - outputLayer.output);
 
         }
         neuron_deltas.add(outputLayerDeltas);
         // handle all other layers - Pas besoin de faire les sorties(déjà faites) et les entrées.
-        for(int j = _network.layers.length - 2; j >= 1 ; j--){
+        for(int j = _network.layers.length - 2; j >= 1 ; --j){
             double [] hiddenLayerDeltas = new double[_network.layers[j].length];
-            for(int k = 0; k < _network.layers[j].length; k++) {
+            for(int k = 0; k < _network.layers[j].length; ++k) {
             
                 hiddenLayerDeltas[k] = computeCurrentHiddenDelta(_network.layers[j][k], neuron_deltas.get(neuron_deltas.size() - 1), _network.layers[j+1], k);
             }
@@ -51,7 +50,7 @@ public class BackPropagation {
      */
     private static double computeCurrentHiddenDelta(Neuron currentNeuron, double[] previousDeltas, Neuron[] previousLayer, int indexCurrentLayer) {
         double sum = 0;
-        for(int i = 0; i < previousDeltas.length; i++) {
+        for(int i = 0; i < previousDeltas.length; ++i) {
             sum += previousLayer[i].weights[indexCurrentLayer] * previousDeltas[i]; //somme de w_ij * delta_j
         }
         return currentNeuron.output * (1 - currentNeuron.output) * sum; // output_i * (1 - output_i) * somme de w_ij * delta_j
@@ -65,12 +64,12 @@ public class BackPropagation {
      */
     public static void update_weights(final NeuralNet _network, final List<double[]> _neuron_deltas){
         // update all layers using _neuron_deltas, starting from the output layer
-        for(int i = _network.layers.length - 1; i > 0 ; i--) {
+        for(int i = _network.layers.length - 1; i > 0 ; --i) {
             Neuron [] curr = _network.layers[i];
             Neuron [] prev = _network.layers[i-1];
             double [] deltas = _neuron_deltas.isEmpty() ? null : _neuron_deltas.remove(0);
             for(int k = 0; k < curr.length; k++) {
-                for(int j = 0; j < curr[k].weights.length; j++) {
+                for(int j = 0; j < curr[k].weights.length; ++j) {
                     curr[k].weights[j] += _network.learningRate * prev[j].output * deltas[k];
                 }
             }
